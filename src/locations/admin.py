aaafb8f31df9de32ac2de5ee080390d21f239fac
fsabre <fabien.sabre@gmail.com>
2024-03-24 13:27:05 +0100

    feat: Add column names on object listings

--- src/locations/admin.py
+++ src/locations/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Location, Category
 
-admin.site.register(Location)
-admin.site.register(Category)
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ["name", "parent"]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Category, CategoryAdmin)
