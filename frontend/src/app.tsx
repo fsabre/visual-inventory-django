53088c55ca1e42fb98445aa7ed44573c652fb352
Matias Sanches <matias.sanchesdasilva@gmail.com>
2025-06-05 11:30:29 +0200

    feat: Add sub-location fonctionnality like - Take his own position/height with the backend Sub-container will be a tile container with a scale factor Change the parent tile to be a div without absolute position

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,9 +1,8 @@
-import {useEffect, useState} from "preact/hooks";
+import { useEffect, useState , useMemo} from "preact/hooks";
+import { Category, Location } from "./models.ts";
+import { getCategories, getLocation } from "./api.ts";
 
-import {Category, Location} from "./models.ts";
-import {getCategories, getLocation} from "./api.ts";
-
-const BLANK_LOCATION: Location = {id: 0, name: "", parent: null, children: [], categories: []};
+const BLANK_LOCATION: Location = { id: 0, name: "", parent: null, children: [], categories: [], x:0, y:0, dx:0, dy:0 };
 
 export function App() {
     const [locationId, setLocationId] = useState(1);
@@ -17,28 +16,40 @@ export function App() {
         })();
     }, [locationId]);
 
+    // Overflow ellipsis sur les tuiles
+    
+    const containerWidth = useMemo(() => Math.max(...location.children.map(child => (child.x) + (child.dx)), 0), [location]) ; 
+    const containerHeight = useMemo(() => Math.max(...location.children.map(child => (child.y) + (child.dy)), 0), [location]) ; 
+
     return (
         <div>
             <h1>Location n°{location.id} ({location.name})</h1>
-            <div
-                onClick={() => setLocationId(location.parent ?? 1)}
-                className={"tile"}
-            >
+            <div onClick={() => setLocationId(location.parent ?? 1)} className="parent">
                 Parent
             </div>
-            <CategorySelector value={searchedCategoryId} onChange={setSearchedCategoryId}/>
+
+            <CategorySelector value={searchedCategoryId} onChange={setSearchedCategoryId} />
+
             <h2>Sub-locations :</h2>
-            <div className={"tile-container"}>
+            <div  className="tile-container"
+                style={{ width: `${containerWidth}px`, height: `${containerHeight}px`   }}>
                 {location.children.map((child, idx) => (
                     <div
                         key={idx}
                         onClick={() => setLocationId(child.id)}
-                        className={child.categories.map(cat => cat.id).includes(searchedCategoryId) ? "tile highlighted" : "tile"}
+                        className={child.categories.some(cat => cat.id === searchedCategoryId) ? "tile highlighted" : "tile"}
+                        style={{
+                            width: `${child.dx}px`,
+                            height: `${child.dy}px`,
+                            left: `${child.x}px`,
+                            top: `${child.y}px`,
+                        }}
                     >
                         {child.name}
                     </div>
                 ))}
             </div>
+
             <h2>Contains :</h2>
             <ul>
                 {location.categories.map(cat => (
@@ -54,20 +65,17 @@ interface CategorySelectorProps {
     onChange: (id: number) => void;
 }
 
-function CategorySelector(props: CategorySelectorProps) {
+function CategorySelector({ value, onChange }: CategorySelectorProps) {
     const [categories, setCategories] = useState<Category[]>([]);
 
     useEffect(() => {
-        getCategories().then(cats => setCategories(cats));
+        getCategories().then(setCategories);
     }, []);
 
     return (
         <div>
             <p>Select a category to look for :</p>
-            <select
-                value={props.value}
-                onChange={ev => props.onChange(Number(ev.currentTarget.value) ?? 0)}
-            >
+            <select value={value} onChange={ev => onChange(Number(ev.currentTarget.value) || 0)}>
                 <option value={0}></option>
                 {categories.map(cat => (
                     <option key={cat.id} value={cat.id}>{cat.name}</option>
